c8f79466ba9e3e5a803294a92493816c1d876780
Victor Kodzo Seayrom <mrseayrom@gmail.com>
2022-03-11 16:19:44 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let firstName = "John";
+let lastName = "Doe";
+let age = 21;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log("My name is " + firstName + " " + lastName +". I am " + age + " " + "years old.");
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old.`);
+let fruits = ["Mango", "Apple", "Orange", "Pineapple", "Pawpaw"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0])
+console.log(fruits.push("Banana"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits);
+console.log(fruits.unshift("Pear"));
+console.log(fruits);
+console.log(fruits.shift());
+console.log(fruits.includes("Mango"));
+console.log(fruits.includes("Ginger"));
+console.log(fruits.join("_"));
